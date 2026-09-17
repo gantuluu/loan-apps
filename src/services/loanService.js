4bdd6f3846764caf2084845a0ed47c0b2a679955
gantuluu <lordgantulu@gmail.com>
2026-09-18 05:40:25 +0800

    chore: empty repository

--- src/services/loanService.js
+++
@@ -1,8 +0,0 @@
-import { activeLoan, loanHistory, loanOffer } from '../data/mock.js';
-
-export const loanService = {
-  getOffer: () => loanOffer,
-  getActiveLoan: () => activeLoan,
-  getHistory: () => loanHistory,
-  calculateInstallment: (amount, tenor, rate = 0.012) => Math.round((amount * (1 + rate)) / tenor),
-};
