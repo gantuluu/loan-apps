b7f408bd072de24c73bc879f59ee5c321dc8b771
gantuluu <lordgantulu@gmail.com>
2026-09-17 16:49:37 +0800

    refactor: use modular app entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App as KonstaApp } from 'konsta/react';
-import App from './App.jsx';
+import App from './app/App.jsx';
 import './styles.css';
 
 function Root() {
